db7385963c34fbb624bf49c22005b03334a50512
RodMiley-ins <rodney.miley@insolvency.gov.uk>
2026-03-29 15:07:53 +0100

    Added v1 contact form

--- app/routes.js
+++ app/routes.js
@@ -52,4 +52,45 @@ router.post('/ipservice/ip-upload/v3/upload-successV3', function (req, res) {
 
 
 
+router.post('/customer-forms/v1/uploadDocumentsQuestion', function(request, response) {
 
+    var uploadDocuments = request.session.data['uploadDocuments']
+    if (uploadDocuments == "Yes"){
+        response.redirect("/customer-forms/v1/upload")
+    } else {
+        response.redirect("/customer-forms/v1/check-your-answers")
+    }
+})
+
+
+
+router.post('/customer-forms/v1/formDetail', function(request, response) {
+
+    var reason = request.session.data['contactReason']
+    if (reason == "General enquiry"){
+        response.redirect("/customer-forms/v1/form-detail-general-enquiry")
+    } 
+    
+    else if (reason == "Complain about an insolvency practitioner"){
+        response.redirect("/customer-forms/v1/form-detail-insolvency-practitioner-complaint")
+    }
+
+      else if (reason == "Investigations and Enforcement Services"){
+        response.redirect("/customer-forms/v1/form-detail-investigations-and-enforcement-services")
+    }
+
+        else if (reason == "Complain about The Insolvency Service"){
+        response.redirect("/customer-forms/v1/form-detail-the-complain-about-the-insolvency-service")
+    }
+
+        else if (reason == "Investigations and Enforcement Services Breach Questionnaire"){
+        response.redirect("/customer-forms/v1/form-detail-investigations-and-enforcement-services-breach-questionnaire")
+    }
+})
+
+
+
+
+
+    
+           
