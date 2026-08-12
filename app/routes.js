70780e1e3fba51deaa7c2fb26b8af9abdce73412
RodMiley-ins <rodney.miley@insolvency.gov.uk>
2026-08-12 14:28:57 +0100

    Added v2 IIR

--- app/routes.js
+++ app/routes.js
@@ -22,6 +22,181 @@ router.post('/individual-insolvency-register/search-the-individual-insolvency-re
   res.redirect('/individual-insolvency-register/search-the-individual-insolvency-register-results-2')
 })
 
+router.post('/individual-insolvency-register/search-the-individual-insolvency-register-results-3', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/search-the-individual-insolvency-register-results-3')
+})
+
+router.post('/individual-insolvency-register/search-the-individual-insolvency-register-results-4', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/search-the-individual-insolvency-register-results-4')
+})
+
+router.post('/individual-insolvency-register/search-the-individual-insolvency-register-results-5', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/search-the-individual-insolvency-register-results-5')
+})
+
+router.post('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results')
+})
+
+router.post('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-2', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-2')
+})
+
+router.post('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-3', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-3')
+})
+
+router.post('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-4', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-4')
+})
+
+router.post('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-5', function (req, res) {
+  req.session.data.searchTerm = req.body.searchTerm || 'Smith'
+  res.redirect('/individual-insolvency-register/v2/search-the-individual-insolvency-register-results-5')
+})
+
+router.get('/individual-insolvency-register/v2/case-details', function (req, res) {
+  const caseId = req.query.caseId || req.session.data.caseDetails?.caseId
+  const caseDetailsMap = {
+    bankruptcy: {
+      caseId: 'bankruptcy',
+      name: 'Amelia Smith',
+      title: 'Ms',
+      gender: 'Female',
+      workStatus: 'Self-employed',
+      dob: '12 March 2003',
+      lastAddress: '14 High Street, London, GBR <br> E1 6AN',
+      previousAddresses: 'No previous addresses found',
+      aliases: 'No other names found',
+      caseName: 'Amelia Smith',
+      type: 'Bankruptcy',
+      arrangementDate: '08/05/2022',
+      status: 'Current',
+      notificationDate: '12/08/2023',
+      practitioner: 'Harriet Lane',
+      firm: 'East Recovery Ltd',
+      practitionerAddress: '2 Tileyard Road, London <br> N7 9AH',
+      practitionerTelephone: '020 1234 5678'
+    },
+    'debt-relief-order': {
+      caseId: 'debt-relief-order',
+      name: 'Benjamin Smith',
+      title: 'Mr',
+      gender: 'Male',
+      workStatus: 'Unemployed',
+      dob: '14 August 1998',
+      lastAddress: '22 Riverside, Liverpool, GBR <br> L1 8JQ',
+      previousAddresses: 'No previous addresses found',
+      aliases: 'No other names found',
+      caseName: 'Benjamin Smith',
+      type: 'Debt Relief Order',
+      arrangementDate: '10/11/2021',
+      status: 'Closed',
+      notificationDate: '09/02/2024',
+      practitioner: 'Sarah Jenkins',
+      firm: 'Riverbank Solutions',
+      practitionerAddress: '7 Bank Street, Liverpool <br> L1 1HB',
+      practitionerTelephone: '0151 555 1234'
+    },
+    'bankruptcy-restrictions-undertaking': {
+      caseId: 'bankruptcy-restrictions-undertaking',
+      name: 'Chloe Smith',
+      title: 'Miss',
+      gender: 'Female',
+      workStatus: 'Part-time',
+      dob: '09 November 1991',
+      lastAddress: '8 Park Lane, Bristol, GBR <br> BS1 5DP',
+      previousAddresses: 'No previous addresses found',
+      aliases: 'No other names found',
+      caseName: 'Chloe Smith',
+      type: 'Bankruptcy restrictions undertaking',
+      arrangementDate: '15/04/2023',
+      status: 'Current',
+      notificationDate: '20/05/2024',
+      practitioner: 'Michael Turner',
+      firm: 'Southfield Advisory',
+      practitionerAddress: '10 Castle Street, Bristol <br> BS1 1HX',
+      practitionerTelephone: '0117 987 6543'
+    },
+    'debt-relief-restrictions-undertaking': {
+      caseId: 'debt-relief-restrictions-undertaking',
+      name: 'Daniel Smith',
+      title: 'Mr',
+      gender: 'Male',
+      workStatus: 'Employed',
+      dob: '27 May 1968',
+      lastAddress: '3 King Street, Manchester, GBR <br> M3 2ER',
+      previousAddresses: 'No previous addresses found',
+      aliases: 'No other names found',
+      caseName: 'Daniel Smith',
+      type: 'Debt relief restrictions undertaking',
+      arrangementDate: '01/01/2024',
+      status: 'Current',
+      notificationDate: '30/06/2024',
+      practitioner: 'Claire Roberts',
+      firm: 'Northfield Insolvency',
+      practitionerAddress: '6 Union Street, Manchester <br> M2 4DJ',
+      practitionerTelephone: '0161 654 3210'
+    },
+    'individual-voluntary-arrangement': {
+      caseId: 'individual-voluntary-arrangement',
+      name: 'Ella Smith',
+      title: 'Ms',
+      gender: 'Female',
+      workStatus: 'Self-employed',
+      dob: '03 February 1974',
+      lastAddress: '18 Queens Road, Leeds, GBR <br> LS1 5QT',
+      previousAddresses: 'No previous addresses found',
+      aliases: 'No other names found',
+      caseName: 'Ella Smith',
+      type: 'Individual Voluntary Arrangement',
+      arrangementDate: '01/02/2024',
+      status: 'Current',
+      notificationDate: '06/03/2024',
+      practitioner: 'Racheal Marsh',
+      firm: 'Riverbank Advisors',
+      practitionerAddress: 'Suite 6, 4th Floor, Universal Square, Manchester <br> M12 6JH',
+      practitionerTelephone: '0161 850 1018'
+    }
+  }
+  const caseDetails = caseDetailsMap[caseId]
+  if (!caseDetails) {
+    return res.redirect('/individual-insolvency-register/search-the-individual-insolvency-register-results')
+  }
+  req.session.data.caseDetails = caseDetails
+  res.render('individual-insolvency-register/v2/case-details')
+})
+
+const staticCaseResultPages = [
+  'case-details-bankruptcy',
+  'case-details-debt-relief-order',
+  'case-details-debt-relief-restrictions-undertaking',
+  'case-details-bankruptcy-restrictions-undertaking',
+  'case-details-individual-voluntary-arrangement'
+]
+
+staticCaseResultPages.forEach((page) => {
+  router.get(`/individual-insolvency-register/v2/${page}.html`, function (req, res) {
+    const { name, trading, postcode, dob } = req.query
+    if (name || trading || postcode || dob) {
+      req.session.data.caseResult = {
+        name: name || req.session.data.caseResult?.name || '',
+        trading: trading || req.session.data.caseResult?.trading || '',
+        postcode: postcode || req.session.data.caseResult?.postcode || '',
+        dob: dob || req.session.data.caseResult?.dob || ''
+      }
+    }
+    res.render(`individual-insolvency-register/v2/${page}`)
+  })
+})
+
 // Route from upload to form-validation-errors
 router.post('/ipservice/ip-upload/v2/upload', function (req, res) {
   // Simulate validation - for demo purposes, always show errors first
